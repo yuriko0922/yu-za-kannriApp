74f81d1b496744484c01300c52240b9887b4a266
yuriko <yrk922@gmail.com>
2019-08-08 20:17:14 +0900

    switch文完了

---
+++ yu-za-App/ViewController2ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController2ViewController.swift
+//  yu-za-App
+//
+//  Created by 津國　由莉子 on 2019/08/08.
+//  Copyright © 2019 yurityann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2ViewController: UIViewController {
+
+    
+    //名前を代入するラベル
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    //趣味を代入するラベル
+    @IBOutlet weak var syumiLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
